54df200a9334fac416be8c607ab97e3612286030
xinhehappy <3308394371@qq.com>
2016-07-08 20:11:26 +0800

    SCOSSever
    SCOSSever

---
+++ src/esd/scos/servlet/LoginValidator.java
@@ -0,0 +1,31 @@
+package esd.scos.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginValidator extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	
+	
+	
+
+}
